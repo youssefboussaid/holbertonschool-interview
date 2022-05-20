23b63e445b5e0fd9c38c7bdd07cb7efa2cbb6f97
youssefboussaid <youssbsd@gmail.com>
2022-05-19 17:47:21 -1100

    lib

---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,14 @@
+#ifndef __SANDPILES__LIB__
+#define __SANDPILES__LIB__
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+int verif(int grid[3][3]);
+static void print_grid(int grid[3][3]);
+
+#endif
+
+
