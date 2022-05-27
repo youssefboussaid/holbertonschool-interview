52e085db10b43045ccf500614e8a8017f1057499
youssefboussaid <youssbsd@gmail.com>
2022-05-26 17:14:40 -1100

    task

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -1,77 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
-
 /**
- * is_palindrome - Gets the length of a prefix substring.
- * @head: head of the linked
- *
- * Return: if list is palandrope return 1
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: pointer to head
+ * Return: 1 if palindrome 0 if not
  */
-
-
 int is_palindrome(listint_t **head)
 {
-	listint_t *p, *q, *first_start, *second_start;
-		p = *head;
-		q = *head;
+	listint_t *tmp = *head;
+	unsigned int count = 0, i = 0;
+	int list[99999];
 
 	if (head == NULL)
 		return (0);
-	if (p->next == NULL || *head == NULL)
+
+	if (*head == NULL)
 		return (1);
-	while (1)
+	while (tmp)
 	{
-		p = p->next->next;
-		if (p == NULL)
-		{
-			second_start = q->next;
-			break;
-		}
-		if (p->next == NULL)
-		{
-			second_start = q->next->next;
-			break;
-		}
-		q = q->next;
+		tmp = tmp->next;
+		count = count + 1;
 	}
-	q->next = NULL;
-	second_start = reverse(second_start);
-	first_start = *head;
-	while (first_start != NULL && second_start != NULL)
+	if (count == 1)
+		return (1);
+
+	tmp = *head;
+	while (tmp)
 	{
-		if (first_start->n == second_start->n)
-		{
-			first_start = first_start->next;
-			second_start = second_start->next;
-		}
-		else
-		{
+		list[i++] = tmp->n;
+		tmp = tmp->next;
+	}
+	for (i = 0; i <= count / 2; i++)
+	{
+		if (list[i] != list[count - i - 1])
 			return (0);
-		}
 	}
 	return (1);
-}
-
-/**
- *reverse  - Gets the length of a prefix substring.
- * @head: head of the linked
- *
- * Return: reversed list
- */
-
-listint_t *reverse(listint_t *head)
-{
-	listint_t *p, *q;
-
-	if (head == NULL)
-		return (head);
-	p = head;
-	q = p->next;
-	if (q == NULL)
-		return (p);
-	q = reverse(q);
-	p->next->next = p;
-	p->next = NULL;
-	return (q);
 }
