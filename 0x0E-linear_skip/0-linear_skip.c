fc4cc7938e9057b316fa4d53260f75ac1b5fbc05
youssefboussaid <youssbsd@gmail.com>
2022-08-04 12:19:35 -1100

    task

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -17,17 +17,15 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
         printf("Value checked at index [%lu] = [%d]\n", tmp->express->index, tmp->express->n);
         if (tmp->express->n >= value)
         {
-            printf("Value found at index [%lu] = [%d]\n", tmp->index, tmp->express->n);
+            printf("Value found between indexes [%lu] and [%ld]\n", tmp->index, tmp->express->index);
             break;
         }
         tmp = tmp->express;
     }
-    if (tmp -> express != NULL)
-    {
-        for (list = tmp ; list->next ; list = list->next)
-        {
-            printf("Value checked at index [%lu] = [%ld]\n", tmp->index, list->index);
-        }
+    if (tmp -> express == NULL)
+    {   
+            for (list = tmp; list->next != NULL; list = list->next);
+            printf("Value found between indexes [%lu] and [%ld]\n", tmp->index, list->index);
     }
     for (list = tmp ; list != tmp->express ; list = list->next)
     {
