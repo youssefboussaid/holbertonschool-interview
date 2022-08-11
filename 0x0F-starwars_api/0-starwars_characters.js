57f5aa4c5835335081881840e1f22a4518eaaf0b
youssefboussaid <youssbsd@gmail.com>
2022-08-10 13:02:30 -1100

    task

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -5,18 +5,18 @@ const url = 'https://swapi-api.hbtn.io/api/films/' + movieId;
 
 
 request(url, async (err, response, body) => {
-    if (err) {
-        console.log(err);
-    }
-    for(const charId of JSON.parse(body).characters) {
-        await new Promise((resolve, reject) => {
-            request(charId , (err, response, body) => {
-                if (err) {
-                    reject(err);
-                }
-                console.log(JSON.parse(body).name);
-                resolve();
-            });
-        });
-    }
+	if (err) {
+		console.log(err);
+	}
+	for(const charId of JSON.parse(body).characters) {
+		await new Promise((resolve, reject) => {
+			request(charId , (err, response, body) => {
+				if (err) {
+					reject(err);
+				}
+				console.log(JSON.parse(body).name);
+				resolve();
+			});
+		});
+	}
 });    
