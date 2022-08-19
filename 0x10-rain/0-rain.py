cf0a9a26a3f20fc37788e6fcb2c87e6f9efed9b2
youssefboussaid <youssbsd@gmail.com>
2022-08-18 20:54:10 -1100

    task

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -4,27 +4,24 @@ list
 """
 
 
-from errno import WSAEDQUOT
-
-
 def rain(walls):
-	"""
-	Args:
-		walls - is a list of non-negative integers.
-	Returns:
-		return 0 if empty.
-	"""
-	n = len(walls)
-	total_water = 0
+    """
+    Args:
+        walls - is a list of non-negative integers.
+    Returns:
+        return 0 if empty.
+    """
+    n = len(walls)
+    total_water = 0
 
-	for i in range(1, n - 1):
-		left = walls[i]
-		for j in range(i):
-			left = max(left, walls[j])
-		right = walls[i]
+    for i in range(1, n - 1):
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+        right = walls[i]
 
-		for j in range(i + 1, n):
-			right = max(right, walls[j])
+        for j in range(i + 1, n):
+            right = max(right, walls[j])
 
-		total_water = total_water + (min(left, right) - walls[i])
-	return total_water
+        total_water = total_water + (min(left, right) - walls[i])
+    return total_water
