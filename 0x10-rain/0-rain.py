ba60ebed85605b77e73ad33fe5a6cf90ed4e0156
youssefboussaid <youssbsd@gmail.com>
2022-08-18 20:11:34 -1100

    task

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+def rain(walls):
+
+    l, r = 0, len(walls) - 1
+
+    Maxleft, Maxright = walls[l], walls[r]
+
+    res = 0
+
+    while l < r:
+        if Maxleft < Maxright:
+            l += 1
+            Maxleft = max(Maxleft, walls[l])
+            res += Maxleft - walls[l]
+        else:
+            r -= 1
+            Maxright = max(Maxright, walls[r])
+            res += Maxright - walls[r]
+    return res
