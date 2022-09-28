d306f091bd0b016e9cbce1aa97c0f4ae3dfde4ab
youssefboussaid <youssbsd@gmail.com>
2022-09-28 12:47:27 -1100

    task

---
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,18 @@
+def rotate_2d_matrix(matrix):
+
+    N = len(matrix)
+ 
+    # Transpose the matrixrix
+    for i in range(N):
+        for j in range(i):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = temp
+ 
+    # swap columns
+    for i in range(N):
+        for j in range(N // 2):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[i][N - j - 1]
+            matrix[i][N - j - 1] = temp
+    return matrix
