9a3c12bf6719cad7633ada8d4283d1f3f2a567f6
youssefboussaid <youssbsd@gmail.com>
2022-11-14 11:44:15 -1100

    task

---
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+island_perimiter
+"""
+
+
+def island_perimeter(grid):
+    peri = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                peri += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    peri -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    peri -= 2
+    return peri
