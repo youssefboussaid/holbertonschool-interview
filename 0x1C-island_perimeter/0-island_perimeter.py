5689c7c27a62a3dbd299e0b42722b27924d77d75
youssefboussaid <youssbsd@gmail.com>
2022-11-19 21:02:07 -1100

    task

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -6,6 +6,22 @@ island_perimiter
 
 
 def island_perimeter(grid):
+    """
+    Method:
+    -------
+        Calculate the perimeter of the island described in grid.
+    Parameters:
+    -----------
+        grid: list of list of integers:
+            -0 represents water
+            -1 represents land
+            -Each cell is square, with a side length of 1
+            -Cells are connected horizontally/vertically (not diagonally).
+            -grid is rectangular, with its width and height not exceeding 100.
+    Returns:
+    --------
+        The perimeter of the island.
+    """
     peri = 0
 
     for i in range(len(grid)):
